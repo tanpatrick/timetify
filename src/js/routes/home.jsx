426851e62fb8212b78d72e07a2d3c13cb80481a2
Patrick Tan <tanpatrickjoseph@gmail.com>
2017-09-20 22:26:32 +1200

    Use modal component to add an event to selected date(s)

--- src/js/routes/home.jsx
+++ src/js/routes/home.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment';
 
+import Dialog from '../components/dialog'
+
 BigCalendar.setLocalizer(
     BigCalendar.momentLocalizer(moment)
 )
@@ -11,12 +13,49 @@ class Home extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            events: []
+            events: [],
+            slots: []
         }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
         return true
+    }
+
+    recordEvents(e) {
+        let dialog = $(e.currentTarget);
+        let remarks = dialog.find('#remarks').val()
+
+        if (!remarks || remarks === '') {
+            return
+        }
+
+        let _this = this;
+        let hours = dialog.find('#hours').val()
+
+        this.state.slots.forEach(selectedSlot => {
+            let title = remarks + ' (' + hours + ')'
+            let slot = {
+                'title': title,
+                'start': selectedSlot,
+                'end': selectedSlot
+            }
+
+            let ctr = 0
+            _this.state.events.forEach(event => {
+                if (JSON.stringify(event) == JSON.stringify(slot)) {
+                    ctr++
+                }
+            })
+
+            if (ctr === 0) {
+                _this.setState({ events: _this.state.events.concat([slot]) })
+            } else {
+                alert('Oops! Looks like the event you\'ve entered already exist')
+            }
+        })
+
+        this.setState({ slots: [] })
     }
 
     render() {
@@ -25,38 +64,75 @@ class Home extends Component {
         return (
             <div>
                 <h3>Welcome, User!</h3>
+
                 <BigCalendar
                     {...this.props}
                     selectable
                     events={this.state.events}
                     views={allViews}
                     onSelectSlot={(slotInfo) => {
-                        slotInfo.slots.forEach(selectedSlot => {
-                            let title = prompt('Please enter the event name')
-                            let slot = {
-                                'title': title,
-                                'start': selectedSlot,
-                                'end': selectedSlot
-                            }
+                        this.setState({ slots: slotInfo.slots })
 
-                            let ctr = 0
-                            this.state.events.forEach(event => {
-                                if (JSON.stringify(event) == JSON.stringify(slot)) {
-                                    ctr++
-                                }
-                            })
+                        let startDate = slotInfo.slots[0]
+                        let endDate = slotInfo.slots[0]
 
-                            if (ctr === 0) {
-                                this.setState({ events: this.state.events.concat([slot]) })
-                            } else {
-                                alert('Oops! Looks like the event you\'ve entered already exist')
-                            }
-                        })
+                        $('#event-dialog')
+                            .find('#start-date')
+                            .val(moment(startDate).format('DD / MMM / YYYY'))
+
+                        if (slotInfo.slots.length > 1) {
+                            endDate = slotInfo.slots[slotInfo.slots.lenght - 1]
+
+                            $('#event-dialog')
+                                .find('#end-date-wrapper').show();
+                            $('#event-dialog')
+                                .find('#end-date')
+                                .val(moment(endDate).format('DD / MMM / YYYY'))
+                        } else {
+                            $('#event-dialog')
+                                .find('#end-date-wrapper')
+                                .hide();
+                        }
+
+                        $('#event-dialog').modal('show');
                     }}
                 />
+
+                <Dialog id="event-dialog"
+                    title="Record"
+                    onClose={this.recordEvents.bind(this)}>
+                    <div>
+                        <form>
+                            <div className="form-group row">
+                                <label className="col-sm-3 col-form-label">Date (start)</label>
+                                <div className="col-sm-9">
+                                    <input id="start-date" type="text" className="form-control" placeholder="Between" readOnly />
+                                </div>
+                            </div>
+                            <div className="form-group row" id="end-date-wrapper">
+                                <label className="col-sm-3 col-form-label">Date (end)</label>
+                                <div className="col-sm-9">
+                                    <input id="end-date" type="text" className="form-control" placeholder="Between" readOnly />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col-sm-3 col-form-label">Remarks</label>
+                                <div className="col-sm-9">
+                                    <input id="remarks" type="text" className="form-control" placeholder="Remarks" />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col-sm-3 col-form-label">No of Hours</label>
+                                <div className="col-sm-9">
+                                    <input id="hours" type="text" className="form-control" placeholder="# of hours" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </Dialog>
             </div>
         )
     }
 }
 
-export default Home;
+export default Home
